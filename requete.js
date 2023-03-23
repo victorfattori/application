de94b9934d28b70789eca5a18918324556fd640c
victorfattori <100805164+victorfattori@users.noreply.github.com>
2023-03-23 20:48:46 +0100

    Update requete.js

--- requete.js
+++ requete.js
@@ -1,10 +1,10 @@
-export function requete(){
+export function launchRaspberry(){
     fetch('http://192.168.1.153:1300', {      //192.168.241.234:1300
   method: 'POST',
   headers: {
     'Content-Type': 'text/plain',
   },
-  body: 'Hello'
+  body: 'raspberry'
 })
 .then(response => {
   // ici, vous pouvez traiter la réponse du serveur
